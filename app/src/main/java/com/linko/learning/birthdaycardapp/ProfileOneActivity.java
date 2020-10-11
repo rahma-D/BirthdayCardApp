110c44838c9142e17bce2ef005dde18b2da68cef
Rahma D <71867406+rahma-D@users.noreply.github.com>
2020-10-11 18:57:22 +0700

    Create ProfileOneActivity.java

---
+++ app/src/main/java/com/linko/learning/birthdaycardapp/ProfileOneActivity.java
@@ -0,0 +1,13 @@
+package com.linko.learning.birthdaycardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_one);
+    }
+}
